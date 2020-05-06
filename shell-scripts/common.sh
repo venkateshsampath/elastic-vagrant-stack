9efee113fefd2c147b483404281d0515718c3363
Venkatesh Sampath <venkateshsampath@users.noreply.github.com>
2020-05-06 16:39:12 +0530

    Update common.sh

--- shell-scripts/common.sh
+++ shell-scripts/common.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# sudo yum install -y telnet vim
-
-# sudo yum -y install java-1.8.0-openjdk
-
-# cat <<EOF | sudo tee /etc/profile.d/java8.sh
-# export JAVA_HOME=/usr/lib/jvm/jre-openjdk
-# export PATH=\$PATH:\$JAVA_HOME/bin
-# export CLASSPATH=.:\$JAVA_HOME/jre/lib:\$JAVA_HOME/lib:\$JAVA_HOME/lib/tools.jar
-# EOF
-
-# source /etc/profile.d/java8.sh
-
 cat <<EOF | sudo tee /etc/yum.repos.d/elasticsearch.repo
 [elasticsearch-7.x]
 name=Elasticsearch repository for 7.x packages
